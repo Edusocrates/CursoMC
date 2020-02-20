829fcfcdcce997425f6da5f2d448b79020960ba5
Eduardo Socrates <edu5390@gmail.com>
2020-02-19 21:19:03 -0300

    Testando a primeira Classe de dominio - Categoria

--- src/main/java/com/pheonixIT/CursoMC/resources/CategoriaResource.java
+++ src/main/java/com/pheonixIT/CursoMC/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.pheonixIT.CursoMC.resources;
 
+import com.pheonixIT.CursoMC.resources.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/Categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String Listar(){
-        return "Rest está funcionando !!";
+    public List<Categoria> Listar(){
+        Categoria cat1 = new Categoria(1,"Informatica");
+        Categoria cat2 = new Categoria(2,"Escritorio");
+        List<Categoria> lista = new ArrayList<Categoria>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+
+        return lista ;
     }
 
 }
