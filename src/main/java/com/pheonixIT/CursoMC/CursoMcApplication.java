f914a91f44f16b41a501e8b17e10a6e8769a8296
Eduardo Socrates <edu5390@gmail.com>
2020-03-22 21:10:14 -0300

    Criando operação de instanciacao

--- src/main/java/com/pheonixIT/CursoMC/CursoMcApplication.java
+++ src/main/java/com/pheonixIT/CursoMC/CursoMcApplication.java
@@ -1,13 +1,30 @@
 package com.pheonixIT.CursoMC;
 
+import com.pheonixIT.CursoMC.repositories.CategoriaRepository;
+import com.pheonixIT.CursoMC.resources.domain.Categoria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+
+	}
 }
